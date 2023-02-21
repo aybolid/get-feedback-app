ae924413a6e43ceacd07f8b0ac5848247897bb3d
Matthew Ivanov <111901125+aybolid@users.noreply.github.com>
2023-02-21 13:59:52 +0200

    seo

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="lorem ipsum" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="main-bg">
