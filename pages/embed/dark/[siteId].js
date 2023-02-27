ed4f09cb4283cc1c1337eb403ade21b6a33bf31f
Matthew Ivanov <111901125+aybolid@users.noreply.github.com>
2023-02-27 19:29:20 +0200

    feedback styles

---
+++ pages/embed/dark/[siteId].js
@@ -0,0 +1,233 @@
+import React, { useEffect, useState } from "react";
+import { getAllSites } from "@/lib/firebase/db-admin";
+import useSWR from "swr";
+import { fetcher } from "@/helpers/fetchers";
+import { useRouter } from "next/router";
+import { format, parseISO } from "date-fns";
+import { useAuth } from "@/lib/firebase/auth";
+import { createFeedback } from "@/lib/firebase/db";
+import { NextSeo } from "next-seo";
+import { Rating } from "react-simple-star-rating";
+import { useForm } from "react-hook-form";
+import { motion } from "framer-motion";
+import { FcGoogle } from "react-icons/fc";
+import { SiGithub } from "react-icons/si";
+
+export async function getStaticProps() {
+  return {
+    props: {
+      feedback: [],
+    },
+    revalidate: 1,
+  };
+}
+
+export async function getStaticPaths() {
+  const sites = await getAllSites();
+  const paths = sites.map((site) => ({
+    params: {
+      siteId: site.id.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+const title = "Embed Feedback - Get Feedback";
+
+const SiteFeedback = ({ feedback }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const { user } = useAuth();
+  const [rating, setRating] = useState(0);
+  const router = useRouter();
+  const [displaySuccess, setDisplaySucces] = useState(false);
+
+  const url = "http://getfb.vercel.app" + router.asPath;
+
+  const { data: approvedFeedback } = useSWR(
+    `/api/feedback/approved/${router.query.siteId}`,
+    fetcher
+  );
+  feedback = approvedFeedback;
+
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
+
+  const handleFeedbackAdd = ({ feedbackContent }) => {
+    const { name, uid, provider } = user;
+    const siteId = router.query.siteId;
+    const ratingValue = rating;
+    const date = new Date().toISOString();
+
+    const newFeedback = {
+      author: name,
+      authorId: uid,
+      provider: provider,
+      siteId: siteId,
+      text: feedbackContent,
+      rating: ratingValue,
+      createdAt: date,
+      status: "pending",
+    };
+
+    createFeedback(newFeedback)
+      .then(() => {
+        setRating(0);
+        reset();
+      })
+      .then(() => setDisplaySucces(true));
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      displaySuccess && setDisplaySucces(false);
+    }, 5000);
+  }, [displaySuccess]);
+
+  return (
+    <>
+      <NextSeo
+        title={title}
+        canonical={url}
+        openGraph={{
+          url,
+          title,
+        }}
+      />
+      <section className="bg-white dark:bg-neutral-800 w-full h-full p-4 mx-auto my-0 text-neutral-700 dark:text-neutral-50">
+        <form onSubmit={handleSubmit(handleFeedbackAdd)}>
+          <div className="flex flex-col justify-center items-start">
+            <label htmlFor="feedbackInput" className="font-semibold text-lg">
+              Send Feedback
+            </label>
+            <div className="w-full relative">
+              <textarea
+                {...register("feedbackContent", {
+                  required: true,
+                  maxLength: 660,
+                })}
+                id="feedbackInput"
+                autoComplete="off"
+                placeholder="Your Feedback..."
+                className="flex-grow w-full py-1 px-2 bg-neutral-100 dark:bg-neutral-700 rounded-md min-h-[32px] resize-y max-h-40"
+              />
+              <div className="absolute -bottom-6 right-0">
+                {errors.feedbackContent &&
+                  errors.feedbackContent.type === "required" && (
+                    <motion.span
+                      animate={{ opacity: 1 }}
+                      initial={{ opacity: 0 }}
+                      transition={{ duration: 0.1 }}
+                      className="text-red-500 font-mono mt-1 text-end"
+                    >
+                      This field is required
+                    </motion.span>
+                  )}
+                {errors.feedbackContent &&
+                  errors.feedbackContent.type === "maxLength" && (
+                    <motion.span
+                      animate={{ opacity: 1 }}
+                      initial={{ opacity: 0 }}
+                      transition={{ duration: 0.1 }}
+                      className="text-red-500 font-mono mt-1 text-end"
+                    >
+                      Max length is 660 digits
+                    </motion.span>
+                  )}
+              </div>
+            </div>
+            <div className="my-4">
+              <Rating
+                initialValue={rating}
+                onClick={handleRating}
+                emptyColor={"#525252"}
+                emptyStyle={{ display: "flex" }}
+                fillStyle={{ display: "-webkit-inline-box" }}
+                transition
+                showTooltip
+                tooltipArray={["Terrible", "Bad", "Average", "Good", "Awesome"]}
+                tooltipStyle={{
+                  backgroundColor: "#404040",
+                  fontWeight: "normal",
+                  padding: 4,
+                }}
+              />
+            </div>
+            <button
+              title="Send your feedback"
+              className="btn mb-4 px-8 primary"
+              type="submit"
+            >
+              Send
+            </button>
+          </div>
+          {displaySuccess && (
+            <motion.p
+              animate={{ height: "min-content", opacity: 1 }}
+              initial={{ height: 0, opacity: 0 }}
+              className="w-full text-center mb-4 bg-green-500 rounded-md text-white font-semibold text-lg"
+            >
+              Your feedback was sent! The comment will be shown as soon as it
+              will be approved.
+            </motion.p>
+          )}
+        </form>
+        <section className="max-h-screen dark:bg-neutral-700 bg-neutral-100 rounded-lg overflow-x-hidden overflow-y-auto">
+          {!feedback ||
+            (feedback.length === 0 && (
+              <p className="w-full text-center py-4 text-lg font-semibold">
+                There is no feedback on this website
+              </p>
+            ))}
+          {feedback?.map((feedback) => (
+            <div className="px-4 my-2" key={feedback.id}>
+              <div className="flex justify-start gap-4 items-center">
+                <p className="font-semibold text-xl">{feedback.author}</p>
+                <div className="mt-1">
+                  {feedback.provider === "google.com" && (
+                    <FcGoogle title="Google" size={20} />
+                  )}
+                  {feedback.provider === "github.com" && (
+                    <SiGithub title="Github" size={20} />
+                  )}
+                </div>
+                <div className="flex-grow flex justify-end items-center">
+                  <p className="text-sm text-neutral-400">
+                    {format(parseISO(feedback.createdAt), "PPpp")}
+                  </p>
+                </div>
+              </div>
+              <div className="flex justify-start items-center">
+                {feedback.rating !== 0 ? (
+                  <Rating
+                    emptyColor={"#525252"}
+                    emptyStyle={{ display: "flex" }}
+                    fillStyle={{ display: "-webkit-inline-box" }}
+                    initialValue={feedback.rating}
+                    readonly
+                    size={20}
+                  />
+                ) : (
+                  <span />
+                )}
+              </div>
+              <p className="text-neutral-900 dark:text-neutral-50 font-medium p-2 my-2 text-md bg-white dark:bg-neutral-800 rounded-lg">
+                {feedback.text}
+              </p>
+            </div>
+          ))}
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default SiteFeedback;
